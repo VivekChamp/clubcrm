9389e798fad45c89262b1c0dca555327629b87a8
Hashir <hashir@bluelynx.qa>
2021-05-12 13:42:22 +0300

    Payment Request tracking

---
+++ club_crm/club_crm/doctype/payment_request_to_pg/payment_request_to_pg.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Blue Lynx and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Payment Request to PG', {
+	// refresh: function(frm) {
+
+	// }
+});
